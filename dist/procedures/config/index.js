e33c7ebaf7edafcad0a363987e24f485107c8e00
Mark Russell <marktheprogrammer17@gmail.com>
2025-12-17 17:36:58 -0500

    save

--- dist/procedures/config/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * config procedures
- */
-export { configInit } from "./init.js";
-export { configAdd } from "./add.js";
-export { configRemove } from "./remove.js";
-export { configGenerate } from "./generate.js";
-export { configValidate } from "./validate.js";
-//# sourceMappingURL=index.js.map
